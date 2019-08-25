587f887da62febe857357995e4a2d500ed12d55b
Erick Rivas <erick_rivas@outlook.com>
2019-08-24 23:55:24 -0500

    Add popup support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+
 import { Provider } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { MuiTheme } from 'settings/MuiTheme';
 
 import App from 'components/App';
+import Popup from 'react-popup';
 import Worker from 'settings/bin/Worker';
 import Store from 'settings/bin/Store';
 
@@ -17,6 +19,7 @@ const muiTheme = MuiTheme();
 ReactDOM.render(
     <Provider store={store}>
         <MuiThemeProvider theme={muiTheme}>
+            <Popup />
             <App />
         </MuiThemeProvider>
     </Provider>,
