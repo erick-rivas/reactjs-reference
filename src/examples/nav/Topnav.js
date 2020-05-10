0f9fc9455a914775bc2fc8397c6e1ca63941f705
Erick Rivas <erick_rivas@outlook.com>
2020-05-09 23:24:19 -0500

    Update libs and Licences

--- src/examples/nav/Topnav.js
+++ src/examples/nav/Topnav.js
@@ -1,13 +1,13 @@
 import React from "react";
 import cx from "classnames";
-import Svg from "react-svg";
+import { ReactSVG } from "react-svg";
 import styles from "resources/css/examples/nav/Topnav.module.css";
 
 function Topnav(props)
 {
   return (
     <div className={styles.module}>
-      <Svg className={styles.menu}
+      <ReactSVG className={styles.menu}
         src={require("resources/icons/ic_menu.svg")} />
     </div>
   );
