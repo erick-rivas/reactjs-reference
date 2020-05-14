cf8f11f4783a635efb25ec094366213b85634119
Erick Rivas <erick_rivas@outlook.com>
2020-05-13 23:00:41 -0500

    :astonished: Include ie support

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-const maxAge = 3600000 * 24; // 1day
+const maxAge = 3600000 * 8; // 8hrs
 app.use(express.static(path.join(__dirname, 'build'), { maxAge: maxAge }));
 app.get("*", (req, res) => {
   res.sendfile(path.join(__dirname, "build", "index.html"));
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
