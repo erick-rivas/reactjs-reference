72a142f219def280c7431647fb40d537d6854054
Erick Rivas <erick_rivas@outlook.com>
2021-02-17 01:07:48 -0600

    Start test module

---
+++ src/tests/views/App.test.js
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react';
+import App from 'views/App';
+
+test('app-test', () => {
+  render(<App />);
+  const linkElement = screen.getAllByText(/Login/i);
+  expect(linkElement[0]).toBeInTheDocument();
+});
