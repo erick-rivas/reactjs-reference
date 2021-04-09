64a06a6a9d6c4add4ba3a2f91c1f2d26efff12db
Erick Rivas <erick_rivas@outlook.com>
2021-04-08 21:04:52 -0500

    Set exec command to bin/test.bat

--- bin/test.bat
+++ bin/test.bat
@@ -3,4 +3,4 @@ echo == Executing test cases
 for /f "delims=" %%i in ('docker-compose -f bin/docker/docker-compose.dev.yml ps --services --filter "status=running"') do set RUNNING=%%i
 IF "%RUNNING%" == "" echo "ERROR: Before executing bin/test.bat, start server with bin/start.bat"
 IF "%RUNNING%" == "" exit 1
-docker-compose -f bin/docker/docker-compose.dev.yml run reactjs /bin/sh -c "npm test"
+docker-compose -f bin/docker/docker-compose.dev.yml exec reactjs /bin/sh -c "npm test"
