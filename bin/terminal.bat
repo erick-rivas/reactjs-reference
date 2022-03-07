23c1352b837f13821de2321cb0aebd6633a4452a
Erick Rivas <erick_rivas@outlook.com>
2022-03-07 13:26:57 -0800

    Include terminal script

---
+++ bin/terminal.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Seed builder
+:: AUTO_GENERATED (Read only)
+
+for /f "delims=" %%i in ('docker-compose -f bin/docker/docker-compose.yml ps --services --filter "status=running"') do set RUNNING=%%i
+IF "%RUNNING%" == "" echo ERROR: Before executing bin/update.bat, start server with bin/start.bat
+IF "%RUNNING%" == "" exit 1
+
+echo == Opening terminal
+docker-compose -f bin/docker/docker-compose.yml exec reactjs_reference_reactjs /bin/sh
---
+++ bin/terminal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Seed builder
+# AUTO_GENERATED (Read only)
+
+RUNNING=$(sudo docker-compose -f bin/docker/docker-compose.yml ps --services --filter "status=running")
+if [ $RUNNING -z ]; then
+  echo "ERROR: Before executing bin/update.sh, start server with bin/start.sh"
+  exit 1
+fi
+
+echo "== Opening terminal"
+sudo docker-compose -f bin/docker/docker-compose.yml exec reactjs_reference_reactjs /bin/sh
