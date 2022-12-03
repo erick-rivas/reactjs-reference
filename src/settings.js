098a75cddd7fa08dcff76705d39b78e2d6a90120
Mauricio Alvarado <mau.alvalop43@gmail.com>
2022-12-02 18:31:24 -0600

    Add GA support

--- src/settings.js
+++ src/settings.js
@@ -7,7 +7,7 @@ let SERVER_URL = "http://localhost:8008"
 let WS_URL = "ws://localhost:8008";
 let GRAPH_URL = SERVER_URL + "/graphql";
 let API_URL = SERVER_URL + "/api";
-let GA_KEY = "G-MPC52X42CG";
+let GA_KEY = "G-0000000000";
 
 // Production
 
@@ -17,7 +17,7 @@ if (IS_PROD) {
   WS_URL = "ws://localhost:8008";
   GRAPH_URL = SERVER_URL + "/graphql";
   API_URL = SERVER_URL + "/api";
-  GA_KEY = "G-MPC52X42CG";
+  GA_KEY = "G-0000000000";
 }
 
 export { GRAPH_URL, API_URL, APP_URL, WS_URL, GA_KEY, IS_PROD };
