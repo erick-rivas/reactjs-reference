c38baea07eb29bddbcd66a9d4826338b58b0092d
erick-rivas <erick_rivas@outlook.com>
2025-07-08 18:48:08 -0700

    Fix LazyRoute component bug

--- src/seed/examples/tests/data.js
+++ src/seed/examples/tests/data.js
@@ -70,7 +70,7 @@ export const API_PLAYERS = [API_PLAYER]
 const GQL_PLAYER_POSITION_ = {
     name: "",
     code: "",
-    stats: `{"expected_goals": 1289.0, "dominant_leg_accuracy": 6251.0}`,
+    stats: `{"expected_goals": 2950.0, "dominant_leg": "reiciendis odit libero enim cursus mattis ipsum, a", "dominant_leg_accuracy": 8349.0}`,
     details: "{}",
 }
 export const GQL_PLAYER_POSITION = {
@@ -93,7 +93,7 @@ export const GQL_PLAYER_POSITION_PAGINATION = {
 export const API_PLAYER_POSITION = {
     name: "",
     code: "",
-    stats: `{"expected_goals": 2176.0, "dominant_leg_accuracy": 9861.0}`,
+    stats: `{"expected_goals": 4108.0, "dominant_leg": "", "dominant_leg_accuracy": 7945.0}`,
     details: "{}",
 }
 export const API_PLAYER_POSITIONS = [API_PLAYER_POSITION]
--- src/seed/helpers/LazyRoute.js
+++ src/seed/helpers/LazyRoute.js
@@ -3,14 +3,14 @@ __Seed builder__
   (Read_only) Component helper
 */
 
-import React, { Suspense } from "react";
-import { Loading, Route, LazyComponent } from "seed/helpers"
+import React from "react";
+import { Route, LazyComponent } from "seed/helpers"
 
 class LazyRoute extends React.Component {
   render() {
     return <Route
       {...this.props}
-      component={(props) => <LazyComponent {...props} />}
+      component={(props) => <LazyComponent component={this.props.component} {...props} />}
     />
   }
 }
