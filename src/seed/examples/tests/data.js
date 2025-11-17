37f616236c0f9d0fb1179172ca35bba792cf58a9
erick-rivas <erick_rivas@outlook.com>
2025-11-16 21:46:29 -0800

    Update libs

--- src/seed/examples/tests/data.js
+++ src/seed/examples/tests/data.js
@@ -70,7 +70,7 @@ export const API_PLAYERS = [API_PLAYER]
 const GQL_PLAYER_POSITION_ = {
     name: "",
     code: "",
-    stats: `{"expected_goals": 6139.0, "dominant_leg": "architecto nec"}`,
+    stats: `{"expected_goals": 5119.0}`,
     details: "{}",
 }
 export const GQL_PLAYER_POSITION = {
@@ -93,7 +93,7 @@ export const GQL_PLAYER_POSITION_PAGINATION = {
 export const API_PLAYER_POSITION = {
     name: "",
     code: "",
-    stats: `{"expected_goals": 3686.0, "dominant_leg": "modi"}`,
+    stats: `{"expected_goals": 7138.0, "dominant_leg": ""}`,
     details: "{}",
 }
 export const API_PLAYER_POSITIONS = [API_PLAYER_POSITION]
